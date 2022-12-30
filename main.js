dcebd6ff745ee24908d37b1ddf6c0548ee6ab852
Hasul79 <hatuk2@mail.ru>
2022-12-31 01:17:22 +0400

    add music

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener("mouseover",play);
+window.addEventListener("click",play);
+function play() { document.getElementById("a").play();
+
+}
+alert("Tap the screen to turn on the music ");
